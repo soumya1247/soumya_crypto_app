6e6476bab14e33958be8405f363267c87daa1734
soumya1247 <soumyadexter7@gmail.com>
2022-06-17 10:02:02 +0530

    Primary commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components';
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path='/' element={<Homepage />} />
+              <Route exact path='/exchanges' element={<Exchanges />} />
+              <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />} />
+              <Route exact path='/coin/:uuid' element={<CryptoDetails />} />
+              <Route exact path='/news' element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse <br />
+            All Rights Reserverd
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsApiHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+    'X-RapidAPI-Key': 'c8156bd397msh6faa567f3446c0dp10009ejsn876ef5b87183'
+}
+
+const  baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+const createRequest = (url) => ({url, headers: cryptoNewsApiHeaders})
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({baseUrl: baseUrl}),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({newsCategory, count}) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        })
+    })
+});
+
+export const {
+    useGetCryptoNewsQuery,
+} = cryptoNewsApi;
+
+// const axios = require("axios");
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://bing-news-search1.p.rapidapi.com/news',
+//   params: {textFormat: 'Raw', safeSearch: 'Off'},
+//   headers: {
+//     'X-BingApis-SDK': 'true',
+//     'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+//     'X-RapidAPI-Key': 'c8156bd397msh6faa567f3446c0dp10009ejsn876ef5b87183'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
